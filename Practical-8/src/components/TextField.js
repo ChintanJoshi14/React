1c5ee3d2ee4fea169eecd20d0db9b29ebcde0afd
ChintanJoshi14 <chintan15joshi@gmail.com>
2022-03-11 12:02:48 +0530

    Implemented antd

--- Practical-8/src/components/TextField.js
+++ Practical-8/src/components/TextField.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './TextField.css';
 import { ErrorMessage ,useField } from 'formik';
+import {Input} from 'antd';
+
 
 class TextField extends React.Component {
     
@@ -12,7 +14,7 @@ render() {
     return(
         <div className='textfield-layout'>
             <label htmlFor={this.props.name}>{this.props.label}</label>
-            <br/><input {...this.props.name === 'password' || this.props.name === 'confirmPassword' ? 'typePass' : 'typeText'} {...this.props.field} {...this.props.label}></input><br/>
+            <br/><Input {...this.props.name === 'password' || this.props.name === 'confirmPassword' ? 'typePass' : 'typeText'} {...this.props.field} {...this.props.label}></Input><br/>
             <ErrorMessage name={this.props.name} />
         </div>
     );
